878a83a2e888a3ea01c165e9b3b694ad0211dc77
Will <will.lucen4@gmail.com>
2019-11-13 00:36:57 -0300

    Projeto inicial com assets para parte de animation e animator

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    private Animator _anim;
+
+    [SerializeField] private KeyCode blinkAction;
+    [SerializeField] private KeyCode moveAction;
+    [SerializeField] private KeyCode scaleAction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(blinkAction))
+        {
+            blink();
+        }
+        
+        if (Input.GetKeyDown(scaleAction))
+        {
+            scale();
+        }
+        
+        if (Input.GetKeyDown(moveAction))
+        {
+            move();
+        }
+    }
+    
+    public void blink()
+    {
+        _anim.SetTrigger("Blink");
+    }
+    
+    public void move()
+    {
+        _anim.SetTrigger("Move");
+    }
+    
+    public void scale()
+    {
+        _anim.SetTrigger("Scale");
+    }
+}
